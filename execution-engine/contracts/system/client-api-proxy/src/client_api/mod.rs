e66649819456f42aecf88e222e742d088441036e
joonho <jhyeom26@gmail.com>
2020-02-19 16:06:24 +0900

    feat: add standard_payment to client_api_proxy

--- execution-engine/contracts/system/client-api-proxy/src/client_api/mod.rs
+++ execution-engine/contracts/system/client-api-proxy/src/client_api/mod.rs
@@ -5,27 +5,33 @@ use alloc::string::String;
 use contract_ffi::{
     contract_api::{account, runtime, system, Error as ApiError},
     unwrap_or_revert::UnwrapOrRevert,
-    value::{account::PublicKey, U512},
+    value::{
+        account::{PublicKey, PurseId},
+        U512,
+    },
 };
 
 use error::Error;
 
-mod proxy_method_names {
-    use super::internal_method_names;
-
-    pub const BOND: &str = internal_method_names::BOND;
-    pub const UNBOND: &str = internal_method_names::UNBOND;
-    pub const TRANSFER_TO_ACCOUNT: &str = "transfer_to_account";
-}
-
-mod internal_method_names {
-    pub const BOND: &str = "bond";
-    pub const UNBOND: &str = "unbond";
+mod method_names {
+    pub mod proxy {
+        use super::internal;
+        pub const BOND: &str = internal::BOND;
+        pub const UNBOND: &str = internal::UNBOND;
+        pub const STANDARD_PAYMENT: &str = "standard_payment";
+        pub const TRANSFER_TO_ACCOUNT: &str = "transfer_to_account";
+    }
+    pub mod internal {
+        pub const BOND: &str = "bond";
+        pub const UNBOND: &str = "unbond";
+        pub const GET_PAYMENT_PURSE: &str = "get_payment_purse";
+    }
 }
 
 pub enum Api {
     Bond(u64),
     Unbond(Option<u64>),
+    StandardPayment(u32),
     TransferToAccount(PublicKey, u64),
 }
 
@@ -36,19 +42,25 @@ impl Api {
             .unwrap_or_revert_with(ApiError::InvalidArgument);
 
         match method_name.as_str() {
-            proxy_method_names::BOND => {
+            method_names::proxy::BOND => {
                 let amount: u64 = runtime::get_arg(1)
                     .unwrap_or_revert_with(ApiError::MissingArgument)
                     .unwrap_or_revert_with(ApiError::InvalidArgument);
                 Api::Bond(amount)
             }
-            proxy_method_names::UNBOND => {
+            method_names::proxy::UNBOND => {
                 let amount: Option<u64> = runtime::get_arg(1)
                     .unwrap_or_revert_with(ApiError::MissingArgument)
                     .unwrap_or_revert_with(ApiError::InvalidArgument);
                 Api::Unbond(amount)
             }
-            proxy_method_names::TRANSFER_TO_ACCOUNT => {
+            method_names::proxy::STANDARD_PAYMENT => {
+                let amount: u32 = runtime::get_arg(1)
+                    .unwrap_or_revert_with(ApiError::MissingArgument)
+                    .unwrap_or_revert_with(ApiError::InvalidArgument);
+                Api::StandardPayment(amount)
+            }
+            method_names::proxy::TRANSFER_TO_ACCOUNT => {
                 let public_key: PublicKey = runtime::get_arg(1)
                     .unwrap_or_revert_with(ApiError::MissingArgument)
                     .unwrap_or_revert_with(ApiError::InvalidArgument);
@@ -76,13 +88,21 @@ impl Api {
 
                 runtime::call_contract(
                     pos_ref,
-                    (internal_method_names::BOND, amount, bonding_purse),
+                    (method_names::internal::BOND, amount, bonding_purse),
                 )
             }
             Self::Unbond(amount) => {
                 let pos_ref = system::get_proof_of_stake();
                 let amount: Option<U512> = amount.map(Into::into);
-                runtime::call_contract(pos_ref, (internal_method_names::UNBOND, amount))
+                runtime::call_contract(pos_ref, (method_names::internal::UNBOND, amount))
+            }
+            Self::StandardPayment(amount) => {
+                let pos_ref = system::get_proof_of_stake();
+                let main_purse = account::get_main_purse();
+                let payment_purse: PurseId =
+                    runtime::call_contract(pos_ref, (method_names::internal::GET_PAYMENT_PURSE,));
+                system::transfer_from_purse_to_purse(main_purse, payment_purse, (*amount).into())
+                    .unwrap_or_revert();
             }
             Self::TransferToAccount(public_key, amount) => {
                 system::transfer_to_account(*public_key, (*amount).into()).unwrap_or_revert();
