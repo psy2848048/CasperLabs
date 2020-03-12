3e9fa8c1899b0ddc46f3409be7b7e567d82f6734
joonho <jhyeom26@gmail.com>
2020-03-12 19:36:13 +0900

    test: add pos-delegation contract
    This contract calls delegation APIs of hdac-pos

---
+++ execution-engine/contracts/test/pos-delegation/src/lib.rs
@@ -0,0 +1,91 @@
+#![no_std]
+
+extern crate alloc;
+
+use alloc::string::String;
+
+use contract::{
+    contract_api::{account, runtime, system},
+    unwrap_or_revert::UnwrapOrRevert,
+};
+use types::{
+    account::{PublicKey, PurseId},
+    ApiError, ContractRef, U512,
+};
+
+#[repr(u16)]
+enum Error {
+    UnknownCommand,
+}
+
+fn delegate(pos: &ContractRef, validator: &PublicKey, amount: &U512, source: PurseId) {
+    runtime::call_contract::<_, ()>(pos.clone(), (POS_DELEGATE, *validator, *amount, source));
+}
+
+fn undelegate(pos: &ContractRef, validator: &PublicKey, amount: Option<U512>) {
+    runtime::call_contract::<_, ()>(pos.clone(), (POS_UNDELEGATE, *validator, amount));
+}
+
+fn redelegate(
+    pos: &ContractRef,
+    src_validator: &PublicKey,
+    dest_validator: &PublicKey,
+    amount: &U512,
+) {
+    runtime::call_contract::<_, ()>(
+        pos.clone(),
+        (POS_REDELEGATE, *src_validator, *dest_validator, *amount),
+    );
+}
+
+const POS_DELEGATE: &str = "delegate";
+const POS_UNDELEGATE: &str = "undelegate";
+const POS_REDELEGATE: &str = "redelegate";
+
+#[no_mangle]
+pub extern "C" fn call() {
+    let pos_pointer = system::get_proof_of_stake();
+
+    let command: String = runtime::get_arg(0)
+        .unwrap_or_revert_with(ApiError::MissingArgument)
+        .unwrap_or_revert_with(ApiError::InvalidArgument);
+
+    match command.as_ref() {
+        POS_DELEGATE => {
+            let validator: PublicKey = runtime::get_arg(1)
+                .unwrap_or_revert_with(ApiError::MissingArgument)
+                .unwrap_or_revert_with(ApiError::InvalidArgument);
+            let amount: U512 = runtime::get_arg(2)
+                .unwrap_or_revert_with(ApiError::MissingArgument)
+                .unwrap_or_revert_with(ApiError::InvalidArgument);
+            let p1 = system::create_purse();
+
+            system::transfer_from_purse_to_purse(account::get_main_purse(), p1, amount)
+                .unwrap_or_revert();
+
+            delegate(&pos_pointer, &validator, &amount, p1);
+        }
+        POS_UNDELEGATE => {
+            let validator: PublicKey = runtime::get_arg(1)
+                .unwrap_or_revert_with(ApiError::MissingArgument)
+                .unwrap_or_revert_with(ApiError::InvalidArgument);
+            let amount: Option<U512> = runtime::get_arg(2)
+                .unwrap_or_revert_with(ApiError::MissingArgument)
+                .unwrap_or_revert_with(ApiError::InvalidArgument);
+            undelegate(&pos_pointer, &validator, amount);
+        }
+        POS_REDELEGATE => {
+            let src_validator: PublicKey = runtime::get_arg(1)
+                .unwrap_or_revert_with(ApiError::MissingArgument)
+                .unwrap_or_revert_with(ApiError::InvalidArgument);
+            let dest_validator: PublicKey = runtime::get_arg(2)
+                .unwrap_or_revert_with(ApiError::MissingArgument)
+                .unwrap_or_revert_with(ApiError::InvalidArgument);
+            let amount: U512 = runtime::get_arg(3)
+                .unwrap_or_revert_with(ApiError::MissingArgument)
+                .unwrap_or_revert_with(ApiError::InvalidArgument);
+            redelegate(&pos_pointer, &src_validator, &dest_validator, &amount);
+        }
+        _ => runtime::revert(ApiError::User(Error::UnknownCommand as u16)),
+    }
+}
