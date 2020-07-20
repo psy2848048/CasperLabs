f083dbb9e45adfed266bb28b2c9718a647d91c47
joonho <jhyeom26@gmail.com>
2020-07-20 23:04:28 +0900

    test: correct the economy test more semantically

--- execution-engine/engine-tests/src/test/hdac_system_contracts/proof_of_stake/economy.rs
+++ execution-engine/engine-tests/src/test/hdac_system_contracts/proof_of_stake/economy.rs
@@ -1,11 +1,11 @@
-use lazy_static::lazy_static;
-
 use std::convert::TryFrom;
 
 use engine_core::engine_state::genesis::GenesisAccount;
 use engine_shared::motes::Motes;
 use engine_test_support::{
-    internal::{utils, ExecuteRequestBuilder, InMemoryWasmTestBuilder, StepRequestBuilder},
+    internal::{
+        utils, ExecuteRequestBuilder, InMemoryWasmTestBuilder, StepRequestBuilder, DEFAULT_PAYMENT,
+    },
     DEFAULT_ACCOUNT_INITIAL_BALANCE,
 };
 use types::{account::PublicKey, bytesrepr::ToBytes, CLValue, Key, U512};
@@ -18,10 +18,6 @@ const METHOD_DELEGATE: &str = "delegate";
 const METHOD_BOND: &str = "bond";
 
 const BIGSUN_TO_HDAC: u64 = 1_000_000_000_000_000_000_u64;
-lazy_static! {
-    static ref GENESIS_TOTAL_SUPPLY: U512 = U512::from(2_000_000_020) * BIGSUN_TO_HDAC;
-    static ref GENESIS_VALIDATOR_STAKE: U512 = U512::from(1_000_000_000) * BIGSUN_TO_HDAC;
-}
 
 fn query_commission_amount(builder: &InMemoryWasmTestBuilder, validator: &PublicKey) -> U512 {
     let pop_uref = builder.get_pos_contract_uref();
@@ -61,27 +57,32 @@ fn should_run_successful_step() {
     const ACCOUNT_1_ADDR: PublicKey = PublicKey::ed25519_from([1u8; 32]);
     const ACCOUNT_2_ADDR: PublicKey = PublicKey::ed25519_from([2u8; 32]);
 
+    const GENESIS_VALIDATOR_STAKE: u64 = 5u64 * BIGSUN_TO_HDAC;
     const ACCOUNT_2_DELEGATE_AMOUNT: u64 = BIGSUN_TO_HDAC;
+    const ACCOUNT_2_BOND_AMOUNT: u64 = BIGSUN_TO_HDAC;
 
     // ACCOUNT_1 and ACCOUNT_2 bond and self-delegate.
     let accounts = vec![
         GenesisAccount::new(
             ACCOUNT_1_ADDR,
             Motes::new(DEFAULT_ACCOUNT_INITIAL_BALANCE.into()),
-            Motes::new(*GENESIS_VALIDATOR_STAKE),
+            Motes::new(GENESIS_VALIDATOR_STAKE.into()),
         ),
         GenesisAccount::new(
             ACCOUNT_2_ADDR,
             Motes::new(DEFAULT_ACCOUNT_INITIAL_BALANCE.into()),
-            Motes::new(*GENESIS_VALIDATOR_STAKE),
+            Motes::new(GENESIS_VALIDATOR_STAKE.into()),
         ),
     ];
+
+    let genesis_total_supply: U512 =
+        U512::from(DEFAULT_ACCOUNT_INITIAL_BALANCE + GENESIS_VALIDATOR_STAKE) * 2;
 
     // ACCOUNT_2 bond additionally
     let bond_request = ExecuteRequestBuilder::standard(
         ACCOUNT_2_ADDR,
         CONTRACT_POS_VOTE,
-        (String::from(METHOD_BOND), U512::from(BIGSUN_TO_HDAC)),
+        (String::from(METHOD_BOND), U512::from(ACCOUNT_2_BOND_AMOUNT)),
     )
     .build();
 
@@ -102,7 +103,7 @@ fn should_run_successful_step() {
             .and_then(|v| CLValue::try_from(v).map_err(|error| format!("{:?}", error)))
             .expect("should have local value.");
         let got: U512 = got.into_t().unwrap();
-        assert_eq!(got, *GENESIS_TOTAL_SUPPLY);
+        assert_eq!(got, genesis_total_supply);
     }
 
     // #2 distribute
@@ -136,19 +137,24 @@ fn should_run_successful_step() {
     )
     .build();
 
-    // get ACCOUNT_1's balance before commission transfer.
-    let account_1 = builder
-        .get_account(ACCOUNT_1_ADDR)
-        .expect("account should exist");
-    let account_1_balance_before = builder.get_purse_balance(account_1.main_purse());
-
     let mut builder = InMemoryWasmTestBuilder::from_result(result);
     let result = builder
         .exec(delegate_request) // #4-1 ACCOUNT_2 to ACCOUNT_1
         .expect_success()
         .commit()
         .step(StepRequestBuilder::default().build()) // #4-2 distribute
-        .exec(claim_commission_request) // #4-3 ACCOUNT_1 claims commission
+        .finish();
+
+    // get ACCOUNT_1's balance before commission transfer.
+    let account_1 = builder
+        .get_account(ACCOUNT_1_ADDR)
+        .expect("account should exist");
+    let account_1_balance_before = builder.get_purse_balance(account_1.main_purse());
+
+    // #4-3 ACCOUNT_1 claims commission
+    let mut builder = InMemoryWasmTestBuilder::from_result(result);
+    let result = builder
+        .exec(claim_commission_request)
         .expect_success()
         .commit()
         .finish();
@@ -160,7 +166,7 @@ fn should_run_successful_step() {
     // get ACCOUNT_1's balance after commission transfer.
     let account_1_balance_after = builder.get_purse_balance(account_1.main_purse());
 
-    assert!(account_1_balance_before < account_1_balance_after);
+    assert!(account_1_balance_before < account_1_balance_after + *DEFAULT_PAYMENT);
 
     // #7 ACCOUNT_1 claims reward.
     let claim_reward_request = ExecuteRequestBuilder::standard(
@@ -190,5 +196,5 @@ fn should_run_successful_step() {
     // get ACCOUNT_1's balance after reward transfer.
     let account_1_balance_after = builder.get_purse_balance(account_1.main_purse());
 
-    assert!(account_1_balance_before < account_1_balance_after);
+    assert!(account_1_balance_before < account_1_balance_after + *DEFAULT_PAYMENT);
 }
