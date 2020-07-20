a26507b48665cbad5a0bb949354d81b22afc03dc
joonho <jhyeom26@gmail.com>
2020-07-20 15:32:56 +0900

    refactor: remove the unused claim_request

--- execution-engine/contracts/hdac-system/pop/src/store/requests/mod.rs
+++ execution-engine/contracts/hdac-system/pop/src/store/requests/mod.rs
@@ -1,9 +1,7 @@
-mod claim;
 mod redelegate;
 mod unbond;
 mod undelegate;
 
-pub use claim::ClaimRequest;
 pub use redelegate::RedelegateRequest;
 pub use unbond::UnbondRequest;
 pub use undelegate::UndelegateRequest;
